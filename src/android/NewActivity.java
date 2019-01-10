e8702fd201471e6ba0d97fba8d0ec3d6e3d8b441
yazeedSamamah <yazeed_samamah@yahoo.com>
2019-01-10 12:41:58 +0200

    add gradle

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -19,7 +19,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
-import android.support.compat.R
+
 public class NewActivity extends Activity {
 
    /* @Override
